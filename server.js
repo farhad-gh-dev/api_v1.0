bed7d6178348f135f31ea280cc6360a81391b6fa
source-architect <source.architect@gmail.com>
2020-12-28 21:25:38 +0330

    database setup completed

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+require("dotenv/config");
 const express = require("express");
+const mongoose = require("mongoose");
 const server = express();
 
 const PORT = process.env.PORT || 3000;
+
+//DATABASE SETUP
+const uri = process.env.DB_ADDRESS;
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db_connection = mongoose.connection;
+db_connection.once("open", () => {
+  console.log("connected to database...");
+});
 
 server.get("/", (req, res) => {
   res.send("hello");
