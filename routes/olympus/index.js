a84e576a7517fa8ee34396f554878562ad6acbfe
source-architect <source.architect@gmail.com>
2021-08-07 10:29:37 +0430

    disable registerQuote and registerGod from olympus route

--- routes/olympus/index.js
+++ routes/olympus/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const route = express.Router();
 
 // const registerQuote = require("./registerQuote");
+// const registerGod = require("./registerGod");
 const getQuotes = require("./getQuotes");
-const registerGod = require("./registerGod");
 const getAllGods = require("./getAllGods");
 const getEachGod = require("./getEachGod");
 
 // route.post("/quote", registerQuote);
+// route.post("/gods", registerGod);
 route.get("/quote", getQuotes);
-route.post("/gods", registerGod);
 route.get("/gods", getAllGods);
 route.get("/gods/:name", getEachGod);
 
