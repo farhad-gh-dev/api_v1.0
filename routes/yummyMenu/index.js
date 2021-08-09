ff6fc719358c922837529f237dd340a98877fdd1
source-architect <source.architect@gmail.com>
2021-08-09 18:11:21 +0430

    removed token check from menu subroutes on yummy menu route

--- routes/yummyMenu/index.js
+++ routes/yummyMenu/index.js
@@ -6,8 +6,8 @@ const getMenu = require("./getMenu");
 const registerMenu = require("./registerMenu");
 const registerOrder = require("./registerOrder");
 
-route.get("/menu/:restaurantName", verifyToken, getMenu);
-route.post("/", verifyToken, registerMenu);
+route.get("/menu/:restaurantName", getMenu);
+route.post("/", registerMenu);
 route.post("/order", verifyToken, registerOrder);
 
 module.exports = route;
